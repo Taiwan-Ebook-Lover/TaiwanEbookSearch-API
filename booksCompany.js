f88edc77c0d1821150a865b070f481f89ad9bbe4
Yuer Lee <passerbyid@gmail.com>
2017-11-28 18:15:44 +0800

    修正博客來價格擷取錯誤的問題

--- booksCompany.js
+++ booksCompany.js
@@ -50,7 +50,7 @@ function _getBooks($) {
       title: $(elem).children('h3').children('a').prop('title'),
       link: `http://www.books.com.tw/products/${$(elem).children('.input_buy').children('input').prop('value')}`,
       priceCurrency: 'TWD',
-      price: parseFloat($(elem).children('.price').children('strong').children('b').text()),
+      price: parseFloat($(elem).children('.price').children('strong').last().children('b').text()),
       about: $(elem).children('p').text().replace(/......\ more\n\t\t\t\t\t\t\t\t/g, ' ...'),
       publisher: $(elem).children('a[rel=mid_publish]').prop('title'),
       authors
