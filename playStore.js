f8ae440a7f9a368b55ad3e4e69d4d0a7c732f122
Yuer Lee <passerbyid@gmail.com>
2017-12-09 13:13:46 +0800

    設定 Play Store 書籍網址的語言與國家

--- playStore.js
+++ playStore.js
@@ -1,6 +1,6 @@
 const rp = require('request-promise-native');
 const cheerio = require('cheerio');
-const url = require('url');
+const { URL, URLSearchParams } = require('url');
 
 function searchBooks(keywords = '') {
   // URL encode
@@ -42,11 +42,18 @@ function _getBooks($, base) {
     // 先抓作者群字串（可能沒有）
     let authors = $(elem).children('.details').children('.subtitle-container').children('a.author').prop('title');
 
+    // resolve book's link
+    let linkUrl = new URL($(elem).children('a').prop('href'), base);
+
+    // 設定書籍網址的語言與國家
+    linkUrl.searchParams.set('gl', 'tw');
+    linkUrl.searchParams.set('hl', 'zh-tw');
+
     let book = {
       // id: $(elem).children('.caption').children('.price-info').children('meta[itemprop=identifier]').prop('content'),
       thumbnail: $(elem).children('.cover').children('.cover-image-container').children('.cover-outer-align').children('.cover-inner-align').children('img').data('cover-small'),
       title: $(elem).children('.details').children('a.title').prop('title'),
-      link: url.resolve(base, $(elem).children('a').prop('href')),
+      link: linkUrl.href,
       priceCurrency: 'TWD',
       price: parseFloat($(elem).children('.details').children('.subtitle-container').children('.price-container').children('.is-price-tag').children('button.price').children('.display-price').text().replace(/\$|,/g, '').replace(/免費/, '0')),
       about: $(elem).children('.details').children('.description').text(),
