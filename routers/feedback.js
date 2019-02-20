f94279ef351e218962d83553f2562da1e3b08f0a
An-Shiang Tsai <ast850328@gmail.com>
2019-02-20 13:40:48 +0800

    Add feature for sending feedback to Trello Board

--- routers/feedback.js
+++ routers/feedback.js
@@ -2,11 +2,16 @@ const express = require('express');
 const feedbackRouter = express.Router();
 const db = require('../db');
 const bot = require('../bot');
+const uaParser = require('ua-parser-js');
+const rp = require('request-promise-native');
 
 const feedbackTypeList = ['error', 'suggestion', 'wish', 'others'];
+const trello = JSON.parse(process.env.TRELLO);
 
 feedbackRouter.post('/', (req, res, next) => {
 
+  // parse user agent
+  const ua = uaParser(req.headers['user-agent']);
   const feedbackDateTime = new Date().toISOString();
   const type = req.body.type ? req.body.type : 'others';
   const name = req.body.name ? req.body.name : '';
@@ -28,17 +33,55 @@ feedbackRouter.post('/', (req, res, next) => {
   }
 
   // 整理一下 feedback
-  const feedback = {feedbackDateTime, type, name, email, body};
+  const feedback = {
+    feedbackDateTime,
+    type,
+    name,
+    email,
+    body,
+    ...ua};
 
   if (db) {
     // insert feedback
     db.insertFeedback(feedback);
   }
 
-  bot.sendMessage(`${JSON.stringify(feedback, null, '  ')}`);
+  const feedbackString = `${JSON.stringify(feedback, null, '  ')}`;
 
-  return res.status(200).send({
-    message: 'Ok.'
+  console.log(trello);
+
+  // send to trello
+
+  const options = {
+    method: 'post',
+    uri: `https://api.trello.com/1/cards`,
+    body: {
+      desc: feedbackString,
+      name: `${feedback.type} - ${feedback._id} - ${feedback.feedbackDateTime}`,
+      idLabels: trello.labels[feedback.type],
+      idList: trello.feedbackList,
+      key: trello.key,
+      token: trello.token,
+      pos: "top"
+    },
+    json: true,
+    resolveWithFullResponse: true,
+    simple: false,
+    gzip: true,
+    timeout: 10000,
+  };
+
+  rp(options).then(() => {
+    bot.sendMessage(feedbackString);
+    return res.status(200).send({
+      message: 'Ok.'
+    });
+  }).catch((error) => {
+    console.log(error);
+    bot.sendMessage(JSON.stringify(error));
+    return res.status(503).send({
+      message: 'Something is wrong...'
+    });
   });
 
 });
